28651d6af5d1900f844b1a82e6a15b238874d8c3
Robert Leifke <r.leifke@gmail.com>
2024-07-20 17:00:26 -0400

    build removed hardhat package

--- packages/app/wagmi.config.ts
+++ packages/app/wagmi.config.ts
@@ -6,14 +6,6 @@ export default defineConfig({
   contracts: [],
   plugins: [
     actions(),
-    hardhat({
-      project: '../hardhat',
-      deployments: {
-        Message: {
-          11155111: '0xF11f180eE37dd6aa7dD08b8C1Cd670fC4DBE0e34',
-        },
-      },
-    }),
     foundry({
       project: '../foundry',
       deployments: {
